6ecbfe2a9e342aee18d4941fdc38cbf42baa4c50
Magus <mr.magus@gmail.com>
2019-05-04 17:30:38 -0500

    disabling STATICFILES_STORAGE for now

--- keeper/settings.py
+++ keeper/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
